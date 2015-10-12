d8a7265cb4c3924f9046a03045d5a5d2e93a67ec
EduardoBJP <eduardo_bjp@yahoo.com.br>
2015-10-12 14:07:37 -0300

    Working on progress bars and declaring buildings

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
+function Building(name){
+	this.name = name;
+	this.number = 0;
+	this.increase = function(ammount){
+		va inc = 1 || ammount;
+		this.number += inc;
+	}
+} 
+
+
 $(document).ready(function(){
-	$('progressbar').progressbar({value: 30});
+	$('.progressbar').progressbar();
 });
+
