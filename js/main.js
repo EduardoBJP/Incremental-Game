ff14f51114cf3973ef440798b1dd6273786eca5a
EduardoBJP <eduardo_bjp@yahoo.com.br>
2015-10-13 12:02:44 -0300

    bower install handlebars

--- js/main.js
+++ js/main.js
@@ -1,14 +1,25 @@
 function Building(name){
 	this.name = name;
 	this.number = 0;
+	
 	this.increase = function(ammount){
-		va inc = 1 || ammount;
+		var inc = 1 || ammount;
 		this.number += inc;
-	}
+	};
+	
 } 
 
+var buildings = [
+	new Building("Farm"),
+	new Building("Pasture"),
+	new Building("Gatherer"),
+	new Building("Hunter")
+]
 
 $(document).ready(function(){
 	$('.progressbar').progressbar();
+	$('button').button().click(function(){
+		console.log('test');
+	});
 });
 
