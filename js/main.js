b6ef886717cf4c7424fad76eccf4e3f6d84b935f
EduardoBJP <eduardo_bjp@yahoo.com.br>
2015-10-13 21:13:32 -0300

    Finished progressbar testing

--- js/main.js
+++ js/main.js
@@ -1,25 +1,74 @@
-function Building(name){
-	this.name = name;
-	this.number = 0;
+$(document).ready(function(){
 	
-	this.increase = function(ammount){
-		var inc = 1 || ammount;
-		this.number += inc;
+/* Building Class
+Encapsulates Building attributes
+see buildingArray for jQuery DOM manipulation on this class
+ */
+
+function Building(name) {
+	this.name = name;
+	var number = 0;
+	var pBarEl;
+	var buttonEl;
+	
+	this.getButtonEl = function (){
+		return buttonEl;
 	};
 	
+	this.setButtonEl = function (el){
+		buttonEl = el;
+	};
+	
+	this.getPBarEl = function (){
+		return pBarEl;
+	};
+	
+	this.setPBarEl = function (el){
+		pBarEl = el;
+	};
+	
+	var increase = function (ammount) {
+		var inc = 1 || ammount;
+		number += inc;
+	};
+	
+	this.handleClick = function () {
+		increase();
+		pBarEl.progressbar({value: number});
+		
+	}
 } 
 
-var buildings = [
-	new Building("Farm"),
-	new Building("Pasture"),
-	new Building("Gatherer"),
-	new Building("Hunter")
-]
 
-$(document).ready(function(){
-	$('.progressbar').progressbar();
-	$('button').button().click(function(){
-		console.log('test');
-	});
+
+/* this object inserts all buildings on the DOM.  
+we also create attributes on each building related to the jQuery elements for ease of access */
+var buildingArray = {
+	buildings: [],
+	
+	/* Initializes the buildings in the building-container div.
+	uses Handlebars template */
+	init: function(){
+		var source   = $("#building-template").html();
+		var bldTemplate = Handlebars.compile(source);
+		var toAppend = "";
+		for (var i = 0; i < this.buildings.length; i++) {
+			toAppend += bldTemplate({name: this.buildings[i].name});
+		}
+		$('#building-container').append(toAppend);
+		
+		for (var i = 0; i < this.buildings.length; i++){
+			this.buildings[i].setButtonEl($('#building-container #' + this.buildings[i].name + ' button').button().on('click', this.buildings[i].handleClick));
+			this.buildings[i].setPBarEl($('#building-container #' + this.buildings[i].name + ' .progressbar').progressbar());
+		}
+		
+	}
+	
+};
+
+buildingArray.buildings.push(new Building("Farm"), new Building("Pasture"),	new Building("Gatherer"),	new Building("Hunter"));
+buildingArray.init();
+
 });
+
 
