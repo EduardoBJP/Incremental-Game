785fe387f61130b336c1d899d6c6f8e3cfb4a999
EduardoBJP <eduardo_bjp@yahoo.com.br>
2015-10-15 00:55:58 -0300

    working on progress bars (progress was made!)

--- js/main.js
+++ js/main.js
@@ -6,18 +6,58 @@ see buildingArray for jQuery DOM manipulation on this class
  */
 
 function Building(name) {
+	var scope = this;
 	this.name = name;
 	this.numBuildings = 0;
+	this.bldSpeedValue = Math.floor((Math.random() * 101) -50);
 	this.progressValue = 0;
 	
+	var bldActive = false;
+	var bldActiveBonus = 10;
+	
+	
+	var bldSpeedValueParts = function(value) {
+		var val = value;
+		var bldSpeedArray  = [];
+		
+		if (bldActive) {val += bldActiveBonus;}
+		
+		if (val <= -50) {
+			bldSpeedArray.push({value: -50, barClass: 'mltpbar-verylow'});
+		} else if (val <= -30) {
+			bldSpeedArray.push({value: 50 + val, barClass: 'mltpbar-neg2'});
+			bldSpeedArray.push({value: -val, barClass: 'mltpbar-verylow'});
+		} else if (val < 0) {
+			bldSpeedArray.push({value: 20, barClass: 'mltpbar-neg2'});
+			bldSpeedArray.push({value: 30 + val, barClass: 'mltpbar-neg1'});
+			bldSpeedArray.push({value: -val, barClass: 'mltpbar-low'});
+		} else {
+			bldSpeedArray.push({value: 20, barClass: 'mltpbar-neg2'});
+			bldSpeedArray.push({value: 30, barClass: 'mltpbar-neg1'});
+			bldSpeedArray.push({value: val, barClass: 'mltpbar-high'});
+		}
+		
+		return bldSpeedArray;
+		
+	};
+	
+		
 	var id = '#' + this.name + ' ';
 	
-	this.labelEl = $(id + '.bld-label').text(this.name);
-	this.multpBarEl = $(id + '.multiprogressbar').multiprogressbar({parts: [{value: 50}]});
-	this.nmbrEl = $(id + '.bld-number').text(this.numBuildings);
-	this.pBarEl = $(id + '.progressbar').progressbar();
 	
-}; 
+	this.labelEl = $(id + '.bld-label').text(this.name);
+	
+	this.multpBarEl = $(id + '.multiprogressbar').multiprogressbar({parts: bldSpeedValueParts(scope.bldSpeedValue)});
+	this.multpBarEl.click(function(){
+		bldActive = true;
+		console.log(this);
+		scope.multpBarEl.multiprogressbar({parts: bldSpeedValueParts(scope.bldSpeedValue)});
+	});
+	
+	this.nmbrEl = $(id + '.bld-number').text(this.numBuildings);
+	this.pBarEl = $(id + '.progressbar').progressbar({value: 0});
+	
+};
 
 
 
